ca620763474cc369dbcbf25d3b67fae45146f832
Joe Matthews <joetymatthews@gmail.com>
2023-04-15 13:42:56 +0100

    new: main class & spring boot dependencies

--- service/build.gradle.kts
+++ service/build.gradle.kts
@@ -1,5 +1,7 @@
 plugins {
     id("java")
+    id("org.springframework.boot") version "2.4.2"
+    id("io.spring.dependency-management") version "1.0.11.RELEASE"
 }
 
 group = "com.joetymatthews.forum"
@@ -9,7 +11,29 @@ repositories {
     mavenCentral()
 }
 
+ext {
+    set("springCloudVersion", "2020.0.0")
+}
+
+dependencyManagement {
+    imports {
+        mavenBom("org.springframework.cloud:spring-cloud-dependencies:${property("springCloudVersion")}")
+    }
+}
+
 dependencies {
+    implementation("org.springframework.boot:spring-boot-starter")
+    implementation("org.springframework.boot:spring-boot-starter-webflux")
+    implementation("org.springframework.boot:spring-boot-starter-actuator")
+    implementation("org.springframework.boot:spring-boot-starter-data-mongodb-reactive")
+
+    compileOnly("org.projectlombok:lombok:1.18.8")
+    annotationProcessor("org.projectlombok:lombok:1.18.8")
+
+    testImplementation("org.mockito:mockito-inline:2.8.47")
+    testImplementation("io.projectreactor:reactor-test")
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testImplementation("de.flapdoodle.embed:de.flapdoodle.embed.mongo")
     testImplementation(platform("org.junit:junit-bom:5.9.1"))
     testImplementation("org.junit.jupiter:junit-jupiter")
 }
