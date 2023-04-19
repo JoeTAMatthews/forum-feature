9340999ede747e1999024982b8237e05719df624
J.Matthews <joetymatthews@gmail.com>
2023-04-19 14:39:25 +0100

    new: add Comment layers for rest api

--- service/src/main/java/com/joetymatthews/forum/thread/entity/Comment.java
+++ service/src/main/java/com/joetymatthews/forum/thread/entity/Comment.java
@@ -1,11 +1,10 @@
 package com.joetymatthews.forum.thread.entity;
 
 import com.devskiller.friendly_id.FriendlyId;
-import com.joetymatthews.forum.thread.dto.ThreadDTO;
+import com.joetymatthews.forum.thread.dto.CommentDTO;
 import com.joetymatthews.forum.thread.model.Rating;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-import org.springframework.data.annotation.Id;
 import org.springframework.data.annotation.PersistenceConstructor;
 import org.springframework.data.mongodb.core.mapping.Document;
 
@@ -14,39 +13,36 @@ import java.util.List;
 
 @Data
 @NoArgsConstructor
-@Document("threads")
-public class Thread {
+@Document("thread_comments")
+public class Comment {
 
-    @Id
     private String id;
-
-    private String title;
     private String content;
-
     private List<Rating> ratings;
-    private List<Comment> comments;
 
+    private String threadId;
+    private String replyCommentId;
     private String userId;
     private long edited;
     private long created;
 
     @PersistenceConstructor
-    public Thread(String id, String title, String content, String userId, long edited, long created) {
+    public Comment(String id, String content, String threadId, String replyCommentId, String userId, long edited, long created) {
         this.id = id;
-        this.title = title;
         this.content = content;
         this.ratings = new ArrayList<>();
-        this.comments = new ArrayList<>();
+        this.threadId = threadId;
+        this.replyCommentId = replyCommentId;
         this.userId = userId;
         this.edited = edited;
         this.created = created;
     }
 
-    public Thread(String title, String content, String userId) {
-        this(FriendlyId.createFriendlyId(), title, content, userId, System.currentTimeMillis(), System.currentTimeMillis());
+    public Comment(String content, String threadId, String userId, String replyCommentId) {
+        this(FriendlyId.createFriendlyId(), content, threadId, replyCommentId, userId, System.currentTimeMillis(), System.currentTimeMillis());
     }
 
-    public Thread(ThreadDTO dto, String userId) {
-        this(dto.title(), dto.content(), userId);
+    public Comment(CommentDTO dto, String userId) {
+        this(dto.content(), dto.threadId(), dto.replyCommentId(), userId);
     }
 }
--- service/src/main/java/com/joetymatthews/forum/thread/model/Rating.java
+++ service/src/main/java/com/joetymatthews/forum/thread/model/Rating.java
@@ -1,4 +1,19 @@
 package com.joetymatthews.forum.thread.model;
 
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
 public class Rating {
+
+    private String icon;
+    private String userId;
+    private long added;
+
+    public Rating(String icon, String userId) {
+        this.icon = icon;
+        this.userId = userId;
+        this.added = System.currentTimeMillis();
+    }
 }
