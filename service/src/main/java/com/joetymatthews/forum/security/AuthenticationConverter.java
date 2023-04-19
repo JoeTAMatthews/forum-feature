b773da85bf2bfb007554ad51613e48bf09704ccc
J.Matthews <joetymatthews@gmail.com>
2023-04-19 01:22:11 +0100

    new: add auth0 security configuration.

---
+++ service/src/main/java/com/joetymatthews/forum/security/AuthenticationConverter.java
@@ -0,0 +1,48 @@
+package com.joetymatthews.forum.security;
+
+import io.netty.util.internal.StringUtil;
+import org.springframework.core.convert.converter.Converter;
+import org.springframework.security.authentication.AbstractAuthenticationToken;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken;
+import org.springframework.util.StringUtils;
+import reactor.core.publisher.Mono;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class AuthenticationConverter implements Converter<Jwt, Mono<AbstractAuthenticationToken>> {
+
+    private String SCOPE_AUTHORITY_PREFIX = "SCOPE_";
+    private List<String> WELL_KNOWN_SCOPE_ATTRIBUTE_NAME = List.of("scope", "scp", "authorities", "permissions");
+
+    @Override
+    public Mono<AbstractAuthenticationToken> convert(Jwt source) {
+        Collection<SimpleGrantedAuthority> authorities = getScopes(source)
+                .stream()
+                .map(authority -> SCOPE_AUTHORITY_PREFIX + authority)
+                .map(SimpleGrantedAuthority::new)
+                .collect(Collectors.toList());
+        return Mono.just(new JwtAuthenticationToken(source, authorities));
+    }
+
+    private Collection<String> getScopes(Jwt source) {
+        List<String> authorities = new ArrayList<>();
+        for (String attribute : WELL_KNOWN_SCOPE_ATTRIBUTE_NAME) {
+            Object scopes = source.getClaim(attribute);
+            if (scopes.getClass() == String.class) {
+                String str = (String) scopes;
+                if (StringUtils.hasText(str)) authorities.addAll(Arrays.asList(str.split(" ")));
+            } else if (scopes.getClass() == Collection.class) {
+                Collection<Object> list = (Collection<Object>) scopes;
+                list.stream().map(Object::toString).forEach(authorities::add);
+            }
+        }
+
+        return authorities;
+    }
+}
---
+++ service/src/main/java/com/joetymatthews/forum/security/SecurityConfiguration.java
@@ -0,0 +1,52 @@
+package com.joetymatthews.forum.security;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.oauth2.core.DelegatingOAuth2TokenValidator;
+import org.springframework.security.oauth2.core.OAuth2TokenValidator;
+import org.springframework.security.oauth2.jwt.*;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+@Configuration
+@EnableWebFluxSecurity
+public class SecurityConfiguration {
+
+    @Value("/${spring.security.oauth2.resourceserver.jwk.issuer-uri}")
+    private String issuerUri;
+
+    @Value("/${auth0.backend_audience}")
+    private String audience;
+
+    @Bean
+    public SecurityWebFilterChain filterChain(ServerHttpSecurity http) {
+        return http.cors()
+                .and()
+                .csrf().disable()
+                .authorizeExchange()
+                .pathMatchers(HttpMethod.OPTIONS).permitAll()
+
+
+
+                .anyExchange().permitAll()
+                .and()
+                .oauth2ResourceServer()
+                .jwt()
+                .jwtAuthenticationConverter(new AuthenticationConverter())
+                .and()
+                .and().build();
+    }
+
+    @Bean
+    public ReactiveJwtDecoder jwtDecoder() {
+        NimbusReactiveJwtDecoder jwtDecoder = (NimbusReactiveJwtDecoder) ReactiveJwtDecoders.fromOidcIssuerLocation(issuerUri);
+        AudienceValidator validator = new AudienceValidator(audience);
+        OAuth2TokenValidator<Jwt> withIssuer = JwtValidators.createDefaultWithIssuer(issuerUri);
+        OAuth2TokenValidator<Jwt> withAudience = new DelegatingOAuth2TokenValidator<>(withIssuer, validator);
+        jwtDecoder.setJwtValidator(withAudience);
+        return jwtDecoder;
+    }
+}
